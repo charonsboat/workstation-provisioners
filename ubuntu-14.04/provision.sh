3a10903f26c639108fcea53d8ca56573aedb9bdd
xcjs <zlowery@xcjs.com>
2015-04-13 21:11:56 -0400

    Added -y flags missing from repositories being added.

--- ubuntu-14.04/provision.sh
+++ ubuntu-14.04/provision.sh
@@ -20,9 +20,9 @@ sudo update-initramfs -u
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-2
 sudo add-apt-repository -y ppa:webupd8team/atom
 sudo add-apt-repository -y ppa:webupd8team/brackets
-sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
+sudo apt-add-repository -y ppa:remmina-ppa-team/remmina-next
 sudo add-apt-repository -y ppa:jerzy-kozera/zeal-ppa
-sudo add-apt-repository ppa:ondrej/php5
+sudo add-apt-repository -y ppa:ondrej/php5
 
 sudo apt-get update
 sudo apt-get install sublime-text atom brackets remmina zeal lamp-server^
